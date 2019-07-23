4cfc1a8eed787125b1a990ac5eb299d5963a2485
Khaled Hossain <md.khaled.eee@gmail.com>
2019-07-23 18:33:52 +0600

    add winston

--- src/config/logger.js
+++ src/config/logger.js
@@ -1,16 +1,30 @@
 const { createLogger, format, transports } = require('winston');
 const { combine, timestamp, label, prettyPrint } = format;
 
-exports.logger = createLogger({
+const logger = createLogger({
   format: combine(
     label({ label: 'app log' }),
     timestamp(),
     prettyPrint()
   ),
   transports: [
-    new transports.Console(),
+    new transports.File({ filename: 'error.log', level: 'error' }),
     new transports.File({
       filename: 'combined.log',
     })
   ]
 });
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new winston.transports.Console({
+    format: winston.format.simple(),
+  }));
+}
+
+logger.stream = {
+  write: (message) => {
+    logger.info(message.trim());
+  },
+};
+
+module.exports = logger;
